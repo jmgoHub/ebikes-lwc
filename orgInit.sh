348d5219cfb0546dbfc5315efce179e1c77a2919
Jim Martin <jmartin@jmgo.me>
2020-06-22 11:07:41 -0700

    update orginit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,6 @@
 sfdx force:org:create -s -f config/project-scratch-def.json
 sfdx force:source:push
 sfdx force:user:permset:assign -n ebikes
+sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:data:tree:import --plan ./data/sample-data-plan.json
 sfdx force:org:open -p lightning/n/Product_Explorer
